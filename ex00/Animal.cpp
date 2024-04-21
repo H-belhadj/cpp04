c2894ddcb2f87faca251c8772ad47c66600bb36c
belhadj haitam <steankir.haitam@gmail.com>
2024-04-21 11:51:59 +0100

    creating constructor and destructor

--- ex00/Animal.cpp
+++ ex00/Animal.cpp
@@ -6,7 +6,33 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 09:05:34 by hbelhadj          #+#    #+#             */
-/*   Updated: 2024/04/20 09:05:35 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2024/04/21 11:51:32 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Animal.hpp"
+
+Animal::Animal()
+{
+    type = "Animal";
+    std::cout << "Animal constructor called" << std::endl;
+}
+
+Animal::Animal(const Animal& other)
+{
+    std::cout << "Animal Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Animal& Animal::operator=(const Animal& other)
+{
+    std::cout << "Animal Copy Assignment operator called" << std::endl;
+    if(this != &other)
+        this->type = other.type;
+    return (*this);
+}
+
+Animal::~Animal()
+{
+    std::cout << "Animal destructor called" << std::endl;
+}
